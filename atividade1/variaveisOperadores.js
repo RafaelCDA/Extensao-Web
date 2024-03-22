9c3589b33408b4902ed97b25cc53a26ad2e09b34
RAFAEL CUTRIM DE ALMEIDA <rafael.cutrim@a.ucb.br>
2024-03-22 10:27:39 -0300

    Atvidade 2

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,11 @@
+var n1 = 22;
+var n2 = 13;
+var ad = n1 + n2;
+var sub = n1 - n2;
+var mult = n1 * n2;
+var div = n1 / n2;
+
+console.log(`Resultado da adição entre ${n1} e ${n2}: ${ad}`);
+console.log(`Resultado da subtração entre ${n1} e ${n2}: ${sub}`);
+console.log(`Resultado da multiplicação entre ${n1} e ${n2}: ${mult}`);
+console.log(`Resultado da divisão entre ${n1} e ${n2}: ${div}`);
